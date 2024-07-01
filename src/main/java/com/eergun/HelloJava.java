8013c330eb131a9bf77c974f9203266eb877ed61
beriote <168163273+beriote@users.noreply.github.com>
2024-07-01 11:05:22 +0300

    asdf

---
+++ src/main/java/com/eergun/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava{
+	public static void main(String[] args){
+		System.out.println("Hello Java 15");
+		double number = 32131231231d;
+		System.out.println(number);
+	}
+}
---
+++ src/main/java/com/eergun/w2d1.java
@@ -0,0 +1,9 @@
+package com.eergun;
+
+public class w2d1 {
+    public static void main(String[] args) {
+        char i = 'a';
+        i += 1;
+        System.out.println(i);
+    }
+}
